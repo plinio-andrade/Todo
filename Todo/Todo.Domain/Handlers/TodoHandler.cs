7a5e2657271a40df453690ffca425e14ef3d10d8
Plinio Andrade <andrade-plinio@outlook.com>
2020-10-31 11:27:00 -0300

    Added handlers, contracts and repositories

---
+++ Todo/Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,38 @@
+﻿using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable,
+        IHandler<CreateTodoCommand>
+    {
+        private readonly ITodoRepository _repository;
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            // Fail Fast Validation
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+
+            // Generate todo
+            var todo = new TodoItem(command.Title, command.User, DateTime.Now);
+
+            // Save in database
+            _repository.Create(todo);
+
+            // Return the result
+            return new GenericCommandResult(true, "Tarefa salva!", todo);
+        }
+    }
+}
