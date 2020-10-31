367511a54939bbf9b5fdbe05975ee0114e1a7f23
Plinio Andrade <andrade-plinio@outlook.com>
2020-10-31 10:21:26 -0300

    Add entities

---
+++ Todo/Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+            Done = false;
+        }
+
+        public string Title { get; private set; }
+        public string User { get; private set; }
+        public DateTime Date { get; private set; }
+        public bool Done { get; private set; }
+
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+
+        public void MarkAsUndone()
+        {
+            Done = false;
+        }
+
+        public void Update(string title)
+        {
+            Title = title;
+        }
+    }
+}
